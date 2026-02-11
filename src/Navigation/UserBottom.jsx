a4bb7fe881ced934d5a7a0313d5e419a8f7e91de
Datta Padre <datta.padre@gmail.com>
2026-02-11 15:00:09 +0530

    BottomTab

--- src/Navigation/UserBottom.jsx
+++ src/Navigation/UserBottom.jsx
@@ -1,15 +1,79 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 import Home from '../Screens/HomeScreen/Home';
+import MyOrder from '../Screens/MyOrder/MyOrder'
+import MyProfile from '../Screens/MyProfile/MyProfile'
+import Favorites from '../Screens/Favorites/Favorites'
+
+const Tab = createBottomTabNavigator();
 
 const UserBottom = () => {
-
-    const Tab = createBottomTabNavigator();
-
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: {
+          height: 75,
+          justifyContent:"center",
+          alignItems:"center"
+        },
+        tabBarLabelStyle: { fontSize: 13 },
+        tabBarActiveTintColor: '#514eb7',
+        tabBarInactiveTintColor: 'gray',
+
+        tabBarIcon: ({ color, size, focused }) => {
+
+          if (route.name === 'Home') {
+            return (
+              <Ionicons
+                name={focused ? 'home' : 'home-outline'}
+                size={size}
+                color={color}
+              />
+            );
+          }
+
+          if (route.name === 'My Order') {
+            return (
+              <MaterialCommunityIcons
+                name={focused ? 'truck-fast': 'truck-fast-outline'}
+                size={size}
+                color={color}
+              />
+            );
+          }
+
+          if (route.name === 'Favorites') {
+            return (
+              <Ionicons
+                name={focused ? 'heart-sharp' : 'heart-outline'}
+                size={size}
+                color={color}
+              />
+            );
+          }
+
+          if (route.name === 'My Profile') {
+            return (
+              <FontAwesome
+                name={focused ? 'user' : 'user-o'}
+                size={size}
+                color={color}
+              />
+            );
+          }
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="My Order" component={MyOrder} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Screen name="My Profile" component={MyProfile} />
     </Tab.Navigator>
   )
 }
